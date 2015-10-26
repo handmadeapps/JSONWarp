d0754a82dd0e85e77ab490cf9f35c9781892bcba
Konstantinos Kontos <kkon@saturatedcolors.net>
2015-10-26 11:54:17 +0200

    Initial Commit

---
+++ JSONWarp/JSONWarp.h
@@ -0,0 +1,19 @@
+//
+//  JSONWarp.h
+//  JSONWarp
+//
+//  Created by Konstantinos Kontos on 26/10/15.
+//  Copyright © 2015 Saturated Colors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONWarp.
+FOUNDATION_EXPORT double JSONWarpVersionNumber;
+
+//! Project version string for JSONWarp.
+FOUNDATION_EXPORT const unsigned char JSONWarpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONWarp/PublicHeader.h>
+
+
---
+++ JSONWarpTests/JSONWarpTests.m
@@ -0,0 +1,39 @@
+//
+//  JSONWarpTests.m
+//  JSONWarpTests
+//
+//  Created by Konstantinos Kontos on 26/10/15.
+//  Copyright © 2015 Saturated Colors. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSONWarpTests : XCTestCase
+
+@end
+
+@implementation JSONWarpTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
